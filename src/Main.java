e17c3a4b88ae3079bcfc2e4ff6b829c0f9b62261
neon0807 <vitaliivorob3@gmail.com>
2022-12-04 18:15:23 +0300

    ДЗ исправил - 2

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,35 @@ public class Main {
     public static void main(String[] args) {
         int pointa1 = 0;
         int pointb1 = 5;
-        int pointa2 = 0;
+        int pointa2 = 1;
         int pointb2 = 5;
 
         if (pointa1 == pointa2 && pointb1 == pointb2) {
             System.out.println("Отрезок пересечения от " + pointa1 + " до " + pointb1);
-        }else {
+        } else {
 
             if (pointa2 > pointb1 || pointa1 > pointb2) {
                 System.out.println("Пересечения нет");
-            }
+            }else {
 
-            if (pointa1 >= pointa2 && pointb1 <= pointb2) {
-                System.out.println("Отрезок пересечения от " + pointa1 + " до " + pointb1);
-            }
+                if (pointa1 >= pointa2 && pointb1 <= pointb2) {
+                    System.out.println("Отрезок пересечения от " + pointa1 + " до " + pointb1);
+                } else {
 
-            if (pointa1 <= pointa2 && pointb1 >= pointb2) {
-                System.out.println("Отрезок пересечения от " + pointa2 + " до " + pointb2);
-            }
+                    if (pointa1 <= pointa2 && pointb1 >= pointb2) {
+                        System.out.println("Отрезок пересечения от " + pointa2 + " до " + pointb2);
+                    } else {
 
-            if (pointa1 >= pointa2 && pointb1 >= pointb2 && pointa1 < pointb2) {
-                System.out.println("Отрезок пересечения от " + pointa1 + " до " + pointb2);
-            }
+                        if (pointa1 >= pointa2 && pointb1 >= pointb2 && pointa1 < pointb2) {
+                            System.out.println("Отрезок пересечения от " + pointa1 + " до " + pointb2);
+                        } else {
 
-            if (pointa1 <= pointa2 && pointb1 <= pointb2 && pointa2 < pointb1) {
-                System.out.println("Отрезок пересечения от " + pointa2 + " до " + pointb1);
+                            if (pointa1 <= pointa2 && pointb1 <= pointb2 && pointa2 < pointb1) {
+                                System.out.println("Отрезок пересечения от " + pointa2 + " до " + pointb1);
+                            }
+                        }
+                    }
+                }
             }
 
             if (pointa1 == pointb2) {
